ad9ef248bcfefc249ff2f803f24caf616ac03238
kvanarsd <kvanarsd@ucsc.edu>
2024-10-06 23:12:09 -0700

    Make Prefabs

--- Assets/Scripts/Parents/ParentManager.cs
+++ Assets/Scripts/Parents/ParentManager.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static Unity.VisualScripting.Metadata;
 
 public class ParentManager : MonoBehaviour
 {
-    public ParentScript Parent;
-    [SerializeField] private ParentsFSM ParentSM;
+    public GameObject ParentPrefab;
+    private List<ParentsFSM> parents = new List<ParentsFSM>();
 
     // other refs
     [SerializeField] private ChildrenManager ChildMan;
@@ -15,7 +16,12 @@ public class ParentManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int i = 0; i < 5; i++)
+        {
+            GameObject parentObj = Instantiate(ParentPrefab);
+            ParentsFSM parentsFSM = parentObj.GetComponent<ParentsFSM>();
+            parents.Add(parentsFSM);
+        }
     }
 
     // Update is called once per frame
