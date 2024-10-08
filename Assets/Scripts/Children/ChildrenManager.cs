e42df36ca915cc52065824c6a364d4fe7615b247
kvanarsd <kvanarsd@ucsc.edu>
2024-10-07 19:29:23 -0700

    Attempt at movement

--- Assets/Scripts/Children/ChildrenManager.cs
+++ Assets/Scripts/Children/ChildrenManager.cs
@@ -12,10 +12,33 @@ public class ChildrenManager : MonoBehaviour
     [SerializeField] private ParentManager ParentMan;
     [SerializeField] private PlayerManager PlayerMan;
 
+    public bool idle = true;
+    public bool walking = false;
+    public bool following = false;
+    public bool hurt = false;
+    public bool treat = false;
+
+    // random timer for states
+    private float timer;
+
+    public float maxChildren;
+    private float numChildren;
+
+    // bounding screen
+    [SerializeField] private float width;
+    [SerializeField] private float screenTop;
+    [SerializeField] private float screenBottom;
+
+    // random select state
+    [SerializeField] private List<string> states;
+    [SerializeField] private List<float> weights;
+
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < 5; i++)
+        numChildren = Random.Range(0f, maxChildren);
+
+        for (int i = 0; i < numChildren; i++)
         {
             GameObject childObj = Instantiate(ChildPrefab);
             ChildrenFSM childrenFSM =childObj.GetComponent<ChildrenFSM>();
@@ -27,5 +50,20 @@ public class ChildrenManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public IEnumerator Walking()
+    {
+
+        // decide how long to walk in this direction
+        timer = Random.Range(2f, 7f);
+
+        // end of timer stop walking
+        yield return new WaitForSeconds(timer);
+  
+      
+        // choose new state
+        string state = SelectState();
+        if (state == "idle") { idle = true; aud.Stop(); } else if (state == "sleep") { sleeping = true; aud.Stop(); } else { StartWalking(); }
     }
 }
