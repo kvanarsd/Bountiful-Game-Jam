bdc1b809873f5a350683eddf5d17527f94f2c58a
ljcui1 <yangyangjiaocui@gmail.com>
2024-10-08 18:55:20 -0700

    allow fsm access to rigidbody and wasd+arrowkey movement

--- Assets/Scripts/Player/PlayerManager.cs
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -39,7 +39,7 @@ public class PlayerManager : MonoBehaviour
 
         if (kicking == false && throwing == false)
         {
-            if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
+            if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.LeftArrow))
             {
                 walking = true;
                 idle = false;
