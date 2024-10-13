2be07e1cc5a406963c7ed8984e21cb2a105d5c8a
ljcui1 <yangyangjiaocui@gmail.com>
2024-10-12 18:55:17 -0700

    BACKGROUND MUSIC

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -9,40 +9,19 @@ public class AudioManager : MonoBehaviour
 
     public PlayerManager PlayMan { get; set; }
 
-    public AudioClip kick;
-    public AudioClip scream;
+    public AudioClip music;
 
-    bool instate;
-    bool togglePlay;
 
     // Start is called before the first frame update
     void Start()
     {
         aud = GetComponent<AudioSource>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (PlayMan.kicking)
-        {
-            aud.clip = kick;
-            aud.loop = false;
-            instate = true;
-        }
-        else
-        {
-            instate = false;
-        }
-
-        if(instate == true && togglePlay == false)
-        {
-            aud.Play();
-            togglePlay = false;
-        }else if(instate == false)
-        {
-            aud.Stop();
-            togglePlay = false;
-        }
+        
     }
 }
